4b87edfd83e44500e9eab82e91936945b30ff169
x7do0 <x7do02@gmail.com>
2026-02-15 15:09:31 +0300

    quick update

--- files/python/lesson-01/challenge1.py
+++ files/python/lesson-01/challenge1.py
@@ -1,1 +1,27 @@
-# Lesson 01 Challenge
+'''
+========= Challenge: 1 =========
+Course: Programming
+Level: 1
+ 
+2 + 3 = 5
+4 * 5 = 20
+
+Hello Hello Hello
+Welcome
+to Python
+=========================
+'''
+
+#*--------------------------------
+
+'''
+========= Challenge: 2 =========
+Shopping List:
+- Apples  x3
+- Bananas x2
+- Oranges x5
+
+Total items: 10
+Thank you!
+=========================
+'''

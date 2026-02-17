6d359d0ffbb0306b84b50683b4aa8752731ea689
x7do0 <x7do02@gmail.com>
2026-02-17 13:41:15 +0300

    quick update

--- files/python/lesson-02/challenge2.py
+++ files/python/lesson-02/challenge2.py
@@ -1,1 +1,33 @@
-# Lesson 02 Challenge
+'''
+========= Challenge: 1 =========
+#g Create variables:
+    age
+    price
+    name
+    is_student 
+
+#g Print the information in this format:
+    Name: ali
+    Age: 20
+    Price: 6
+    Student: True
+=========================
+'''
+
+
+#*--------------------------------
+
+'''
+========= Challenge: 2 =========
+#g Create variables:
+    fruits (list)
+    coordinates (tuple)
+    user_data (dictionary)
+
+#g Print:
+    First fruit: first element in fruits # ركز: فقط الاول
+    Location: coordinates
+    Username: user_data
+========================= 
+'''
+

e662c179deae864e42e673605f7a04341a17eefd
x7do0 <x7do02@gmail.com>
2026-02-18 20:25:20 +0300

    quick update

--- files/python/lesson-03/challenge3.py
+++ files/python/lesson-03/challenge3.py
@@ -1,1 +1,62 @@
-# Lesson 03 Challenge
+'''
+========= Challenge: 1 ========= 
+#g Create a variable:
+    score = 20
+ 
+#g Apply the following operations in order:
+    Add 10 to score 
+    Multiply score by 2 
+    Subtract 5 from score 
+    Divide score by 5 
+
+#g Print the final score like this:
+    Final Score: score
+=========================
+'''
+score = 20
+score += 10
+score *= 2
+score -= 5
+score /= 5
+
+print("Final Score:", score)
+
+#*--------------------------------
+
+'''
+========= Challenge: 2 =========
+#g Create variables:
+    a = 17
+    b = 5
+
+#g Print the following results:
+    Addition
+    Multiplication
+    Power (a raised to b)
+    Integer Division
+    Remainder
+=========================
+'''
+
+#*--------------------------------
+
+'''
+========= Challenge: 3 =========
+#g Create variables:
+    score = 75
+    attendance = 80
+
+#g Check and print the following:
+    Is the student passed? 
+    (The student passes if score is greater than or equal to 50
+     AND attendance is greater than or equal to 75)
+
+#g Expected output:
+    True or False 
+=========================
+'''
+
+score = 75
+attendance = 80
+
+print()

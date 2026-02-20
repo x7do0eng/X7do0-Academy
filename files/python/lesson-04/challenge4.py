6339a405a21cfd05d373cc5fdbbeb870794d76dd
x7do0 <x7do02@gmail.com>
2026-02-21 00:00:03 +0300

    quick update

--- files/python/lesson-04/challenge4.py
+++ files/python/lesson-04/challenge4.py
@@ -1,1 +1,24 @@
-# Lesson 04 Challenge
+'''
+========= Challenge: 1 =========
+#g Ask the user for:
+    name
+    department 
+
+#g Print:
+    Hello (name)
+    Welcome to department (department)
+========================= 
+'''
+
+#*--------------------------------
+
+'''
+========= Challenge: 2 =========
+#g Ask the user for (Store them in variables):
+    first_name 
+    last_name  
+
+#g Print:
+    Welcome first_name last_name 
+=========================
+'''
--- files/python/lesson-05/challenge5.py
+++ files/python/lesson-05/challenge5.py
@@ -1,1 +1,36 @@
-# Lesson 05 Challenge
+'''
+========= Challenge: 1 =========
+#g Ask the user for: 
+    age
+
+#g Convert age to integer
+
+#g Print:
+    Your age now: age
+    Your age next year: age + 1
+    Type of age: type(age)
+=========================
+'''
+age = input("Enter your age: " )
+age = int(age)
+
+print("Your age now:", age)
+print("Your age next year:", age + 1)
+print("Type of age:", type(age)) 
+
+#*--------------------------------
+
+'''
+========= Challenge: 2 =========
+#g Ask the user for:
+    name
+    number_text
+
+#g Convert number_text to integer
+
+#g Print:
+    Hello name
+    Number length: len(number_text) 
+    Number after adding 5: number_text + 5
+=========================
+'''

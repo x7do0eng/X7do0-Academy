93c02e885079f83b67fc0d4093e80e11a8eb8890
x7do0 <x7do02@gmail.com>
2026-02-26 15:42:31 +0300

    quick update

--- files/python/lesson-06/challenge6.py
+++ files/python/lesson-06/challenge6.py
@@ -1,1 +1,23 @@
-# Lesson 06 Challenge
+'''
+========= Challenge: 1 =========
+#g Ask the user for (Convert the input to integer):
+    age 
+
+#g Print:
+    Your age next year: age + 1
+    Type of age: type(age)
+=========================
+'''
+
+#*--------------------------------
+
+
+'''
+========= Challenge: 2 =========
+#g Create a variable:
+    number = 123.45
+
+#g Print (Using function nesting):
+    Length of number after converting to string
+=========================
+'''

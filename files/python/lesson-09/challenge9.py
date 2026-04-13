154a030a53eb2d1ed2b820b4c63b19cd24d60c52
x7do0 <x7do02@gmail.com>
2026-04-13 13:31:39 +0300

    quick update

--- files/python/lesson-09/challenge9.py
+++ files/python/lesson-09/challenge9.py
@@ -1,1 +1,61 @@
-# Lesson 09 Challenge
+'''
+========= Challenge: 1 ========= 
+#g Create a function:
+    welcome() 
+#o The function should print:
+    Welcome to Python Functions 
+
+#g Call the function
+=========================
+''' 
+
+#*--------------------------------
+
+
+'''
+========= Challenge: 2 =========     
+#g Create a function:             
+    multiply_numbers(num1, num2)     
+#o The function should:           
+    Multiply the two numbers        
+    Return the result             
+                                    
+#g Print the returned value        
+=========================           
+'''
+
+
+#*--------------------------------
+
+
+'''
+========= Challenge: 3 =========
+#g Create a function:
+    check_score(score)
+#o Using conditionals inside the function:
+#o Return:
+    "Pass" if score >= 50
+    "Fail" otherwise
+
+#g Print the returned result
+=========================
+'''
+
+#*--------------------------------
+
+
+'''
+========= Challenge: 4 =========
+#g Create a function:
+    calculate_total(price, quantity)
+#o The function should:
+    Calculate total price
+    Return the result
+
+#g Ask the user for (Convert inputs to numbers using function nesting):
+    price
+    quantity
+
+#g Print the final total 
+=========================
+'''
--- files/python/lesson-09/subject9.py
+++ files/python/lesson-09/subject9.py
@@ -1,1 +1,39 @@
-# Lesson 09 Subject
+# ---------------------------------------------------------
+#w 9. الدوال المخصصة - Custom Functions 
+# ---------------------------------------------------------
+
+#g 1. الدالة الأساسية (Basic: def NAME():) 
+# لتعريف دالة جديدة def نستخدم كلمة 
+def function(): 
+    print("Hello! This is a basic function.") 
+    
+function() 
+
+
+#g 2. الدوال مع الباراميترات والقيم المسترجعة (With Return & Params) 
+# الباراميترات (Parameters): هي البيانات اللي نرسلها للدالة لتعالجها
+#o Return: هي النتيجة اللي ترجعها لنا الدالة بعد الانتهاء              
+
+def add_numbers(num1, num2): 
+    result = num1 + num2     
+    return result                    
+
+# استخدام الدالة وتخزين نتيجتها في متغير
+total = add_numbers(10, 20) 
+print("The sum is:", total) 
+
+
+#o  مثال يجمع الفكرتين (logicالباراميترات والـ )
+def check_age(age): 
+    if age >= 18:    
+        return "Adult" 
+    else:
+        return "Minor" 
+
+status = check_age(5) 
+print("User status:", status) 
+
+# ---------------------------------------------------------
+#. def NAME(): هي الطريقة الرسمية لبدء تعريف أي دالة.
+#. Return: ينهي تنفيذ الدالة ويرسل القيمة للخارج.
+# ---------------------------------------------------------

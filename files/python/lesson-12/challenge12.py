85421f072057bd6e1e242de9c0b580610798a9ce
x7do0 <x7do02@gmail.com>
2026-05-31 15:47:37 +0300

    quick update

--- files/python/lesson-12/challenge12.py
+++ files/python/lesson-12/challenge12.py
@@ -1,1 +1,120 @@
-# Lesson 12 Challenge
+'''
+========= Challenge: 1 =========
+#g Create a dictionary:
+    student = {
+        "name": "Ali",
+        "age": 20
+    }
+
+#g Print:
+    The value of "name"
+    The value of "age" using get
+=========================
+'''
+
+
+#*--------------------------------
+
+
+'''
+========= Challenge: 2 =========
+#g Create a dictionary:
+    data = {
+        "x": 10,
+        "y": 20,
+        "z": 30
+    }
+
+#g Remove:
+    Key "y" using pop
+
+#g Print the dictionary
+=========================
+'''
+
+
+#*--------------------------------
+
+
+'''
+========= Challenge: 3 =========
+#g Create a dictionary:
+    info = {
+        "a": 1,
+        "b": 2,
+        "c": 3
+    }
+
+#g Delete:
+    Key "b" using del
+
+#g Print the dictionary
+=========================
+'''
+
+
+#*--------------------------------
+
+
+'''
+========= Challenge: 4 =========
+#g Create a dictionary:
+    user = {
+        "name": "Omar",
+        "age": 25
+    }
+
+#g Loop using for:
+    Print all keys and values
+=========================
+'''
+
+
+#*--------------------------------
+
+
+'''
+========= Challenge: 5 =========
+#g Create two sets:
+    a = {1, 2, 3}
+    b = {3, 4, 5}
+
+#g Print:
+    Union (a | b)
+    Intersection (a & b)
+=========================
+'''
+
+
+#*--------------------------------
+
+
+'''
+========= Challenge: 6 =========
+#g Create a set:
+    numbers = {10, 20, 30}
+
+#g Add:
+    40
+
+#g Remove:
+    20
+
+#g Print the set
+=========================
+'''
+
+
+#*--------------------------------
+
+
+'''
+========= Challenge: 7 =========
+#g Create two sets:
+    a = {1, 2, 3, 4}
+    b = {3, 4}
+
+#g Print:
+    Difference (a - b)
+=========================
+'''
